8637367c5107a1acf03951b587de1a0f64a8b601
Unknown <bobiyanoni@gmail.com>
2018-04-05 18:06:50 +0300

    Added Read16 abs mode

--- Addressing.c
+++ Addressing.c
@@ -1,14 +1,18 @@
 #include "include/Addressing.h"
 
-uint8_t READ8_ZP(ROM *rom, RAM *ram, uint16_t rom_addr){
+uint8_t READ8_ZP(RAM *ram, uint16_t addr){
     // This is for the Zero-page Addressing mode, The zero page is the first 256 bytes of the RAM.
     // So when we refer to Zero-page addressing, we get the address of the ram from the rom and the value
     // Sitting there.
-    uint8_t zp_address = ROM_READ8(rom, rom_addr);
-
-    return ram->memory[zp_address];
+    return ram->memory[addr];
 }
 
-uint8_t READ_ABS(RAM *ram, uint16_t addr){
+uint8_t READ8_ABS(RAM *ram, uint16_t addr){
     return ram->memory[addr];
+}
+
+uint16_t READ16_ABS(RAM *ram, uint16_t addr){
+    uint8_t lower = ram->memory[addr];
+    uint8_t higher = ram->memory[addr + 1];
+    return (higher << 8) | (lower & 0xff);
 }
