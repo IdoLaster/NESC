c0c9f41d0b4d3701d57936c9394bf9fddd0e8bf9
Unknown <bobiyanoni@gmail.com>
2018-04-05 18:05:40 +0300

    Hardcoded NES Test and modify logging.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
     ROM *rom = malloc(sizeof *rom);
     RAM *ram = malloc(sizeof *ram);
 
-    rom->path = "/share/NES/NESC/ROMS/SMB.nes";
+    rom->path = "/share/NES/NESC/ROMS/nestest.nes";
     if(!load_rom(rom)){
         printf("ROM Loaded incorrectly.\n");
         return 0;
@@ -20,14 +20,15 @@ int main(int argc, char *argv[]){
 
     cpu->rom = rom;
     cpu->ram = ram;
+    cpu->registers.pc = 0xc000;
     power_up(cpu);
-    printf("PC:0x%x, A:0x%x, Y:0x%x, X:0x%x, Status:0x%x\n", cpu->registers.pc, cpu->registers.a, cpu->registers.y,
-           cpu->registers.x, cpu->registers.status);
+    printf("PC:0x%x, A:0x%x, Y:0x%x, X:0x%x, Status:0x%x, SP: 0x%x, Opcode: ", cpu->registers.pc, cpu->registers.a, cpu->registers.y,
+           cpu->registers.x, cpu->registers.status, cpu->registers.sp);
     int run = 1;
     while(run){
         run = cpu_step(cpu);
-        printf("PC:0x%x, A:0x%x, Y:0x%x, X:0x%x, Status:0x%x, Opcode: ", cpu->registers.pc, cpu->registers.a, cpu->registers.y,
-               cpu->registers.x, cpu->registers.status);
+        printf("PC:0x%x, A:0x%x, Y:0x%x, X:0x%x, Status:0x%x, SP: 0x%x, Opcode: ", cpu->registers.pc, cpu->registers.a, cpu->registers.y,
+        cpu->registers.x, cpu->registers.status, cpu->registers.sp);
     }
     return 1;
 }
