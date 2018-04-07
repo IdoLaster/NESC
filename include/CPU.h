4fa8a945e75503229a3d4ddbe8265bf856173c0a
Unknown <bobiyanoni@gmail.com>
2018-04-07 20:57:55 +0300

    Added a bunch of macros to work with flags

--- include/CPU.h
+++ include/CPU.h
@@ -4,10 +4,13 @@
 #ifndef NES_CPU_H
 #define NES_CPU_H
 
+// Some macros to more easliy work with the status flag register thingy.
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+#define CLEAR_BIT(var,pos) var &= ~(1 << (pos))
+#define SET_BIT(var,pos) var |= (1 << pos)
 
-#define ZEROSET(flag) CHECK_BIT(flag,0)
-#define CARRYSET(flag) CHECK_BIT(flag,1)
+#define ZEROSET(flag) CHECK_BIT(flag,1)
+#define CARRYSET(flag) CHECK_BIT(flag,0)
 #define INTERRUPTDISABLE(flag) CHECK_BIT(flag,2)
 #define DECIMALMODESET(flag) CHECK_BIT(flag,3)
 #define OVERFLOWSET(flag) CHECK_BIT(flag,6)
@@ -15,6 +18,21 @@
 
 #define FIXZERO(val, flag) {if(val==0){flag=flag|0b10;}else{flag=flag&0b11111101;}}
 
+#define SETOVERFLOW(flag) SET_BIT(flag, 6)
+#define SETNEGATIVE(flag) SET_BIT(flag, 7)
+#define SETDECIMALMODE(flag) SET_BIT(flag, 3)
+#define SETINTERRUPTDISABLE(flag) SET_BIT(flag, 2)
+#define SETZERO(flag) SET_BIT(flag, 1)
+#define SETCARRY(flag) SET_BIT(flag, 0)
+
+#define CLEAROVERFLOW(flag) CLEAR_BIT(flag, 6)
+#define CLEARNEGATIVE(flag) CLEAR_BIT(flag, 7)
+#define CLEARDECIMALMODE(flag) CLEAR_BIT(flag, 3)
+#define CLEARINTERRUPTDISABLE(flag) CLEAR_BIT(flag, 2)
+#define CLEARZERO(flag) CLEAR_BIT(flag, 1)
+#define CLEARCARRY(flag) CLEAR_BIT(flag, 0)
+
+#define FIXNEGATIVE(val, flag) {if(CHECK_BIT(val,7)){SETNEGATIVE(flag);}else{CLEARNEGATIVE(flag);}}
 #include <stdint.h>
 #include "ROM.h"
 #include "RAM.h"
