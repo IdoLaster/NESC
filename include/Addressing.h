5640f1b09e3280c6e9491ca1bcd7072e04ad2ade
Unknown <bobiyanoni@gmail.com>
2018-04-07 20:58:13 +0300

    Fixed write and read ZP

--- include/Addressing.h
+++ include/Addressing.h
@@ -5,7 +5,8 @@
 #ifndef NES_ADDRESSING_H
 #define NES_ADDRESSING_H
 
-uint8_t READ8_ZP(RAM *ram, uint16_t addr);
+uint8_t READ8_ZP(RAM *ram, uint8_t addr);
+void WRITE8_ZP(RAM *ram, uint8_t addr, uint8_t value);
 uint8_t READ8_ABS(RAM *ram, uint16_t addr);
 uint16_t READ16_ABS(RAM *ram, uint16_t addr);
 #endif //NES_ADDRESSING_H
