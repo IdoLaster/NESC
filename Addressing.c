8b10655bf2140c57e481a879a375f93de638861e
Unknown <bobiyanoni@gmail.com>
2018-04-20 12:28:23 +0300

    Add write 8 indirect x

--- Addressing.c
+++ Addressing.c
@@ -32,3 +32,15 @@ uint16_t READ8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register){
     uint8_t value = READ8(ram, address);
     return value;
 }
+
+void WRITE8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register, uint8_t value){
+    uint8_t lower_location = ram->memory[addr] + x_register;
+    uint8_t lower = READ8(ram, lower_location);
+    uint8_t higher_location = ram->memory[addr] + x_register + 1;
+    if(higher_location % 0x100 == 0){
+        higher_location = lower_location - 0xFF;
+    }
+    uint8_t higher = READ8(ram, higher_location);
+    uint16_t address = (higher << 8) | (lower & 0xff);
+    WRITE8(ram, address, value);
+}
--- include/Addressing.h
+++ include/Addressing.h
@@ -11,5 +11,6 @@ uint8_t READ8_ZP(RAM *ram, uint8_t addr);
 void WRITE8_ZP(RAM *ram, uint8_t addr, uint8_t value);
 uint8_t READ8_ABS(RAM *ram, uint16_t addr);
 uint16_t READ8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register);
+void WRITE8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register, uint8_t value);
 uint16_t READ16_ABS(RAM *ram, uint16_t addr);
 #endif //NES_ADDRESSING_H
