d49210a04c50fddfdfbfcb6f50ca7750b730642b
Unknown <bobiyanoni@gmail.com>
2018-04-21 16:34:42 +0300

    Add indirect Y

--- include/Addressing.h
+++ include/Addressing.h
@@ -11,6 +11,8 @@ uint8_t READ8_ZP(RAM *ram, uint8_t addr);
 void WRITE8_ZP(RAM *ram, uint8_t addr, uint8_t value);
 uint8_t READ8_ABS(RAM *ram, uint16_t addr);
 uint16_t READ8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register);
+uint16_t READ8_INDIRECT_Y(RAM *ram, uint16_t addr, uint8_t y_register);
 void WRITE8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t x_register, uint8_t value);
+void WRITE8_INDIRECT_X(RAM *ram, uint16_t addr, uint8_t y_register, uint8_t value);
 uint16_t READ16_ABS(RAM *ram, uint16_t addr);
 #endif //NES_ADDRESSING_H
